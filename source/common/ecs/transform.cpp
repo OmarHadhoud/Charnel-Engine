2db0e8e8119562a8012ea2b49ce34d7101a4ec94
Omar Salem Hadhoud <omarhadhoud@ymail.com>
2021-12-10 00:08:52 +0200

    make transform requirement (req 2)

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -10,7 +10,25 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: Write this function
-        return  glm::mat4(1.0f); 
+
+        // note: the order of operations is scaling, rotation, translation: new_pos = translation * rotation * scaling * original_pos
+        // but here we are doing translation, rotation, scaling in glm, as glm does the operations in the following way:
+        // if we do glm::scale(translation_matrix, scale)
+        // it calculates the scale matrix first, then multiplies it with the translation matrix: return translation_matrix * scale_matrix
+        // which is the required, i.e.: read glm operations from down to up
+
+
+        // create an identity matrix to use it for the operations of the transform
+        glm::mat4 mat = glm::mat4(1.0f);
+        // translate this identity matrix to the position of the transform
+        mat = glm::translate(mat, this->position);
+        // calculate the rotation matrix from the euler angles
+        glm::mat4 rot = glm::yawPitchRoll(this->rotation.y, this->rotation.x, this->rotation.z);
+        // update the transformation matrix by multiplying it with the rotation matrix
+        mat = mat * rot;
+        // finally, scale the matrix by the scale of the transform
+        mat = glm::scale(mat, this->scale);
+        return mat;
     }
 
      // Deserializes the entity data and components from a json object
