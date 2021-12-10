ecddab877f567e6f38cbe7de98b92b3c02e86052
Omar Salem Hadhoud <omarhadhoud@ymail.com>
2021-12-10 21:11:30 +0200

    remove unnecessary texture configuration that is not required

--- source/common/texture/texture2d.hpp
+++ source/common/texture/texture2d.hpp
@@ -12,20 +12,8 @@ namespace our {
         // This constructor creates an OpenGL texture and saves its object name in the member variable "name" 
         Texture2D() {
             //TODO: Complete this function
-            // First generate the texture on the GPU
+            //  Generate the texture on the GPU and save its id in name
             glGenTextures(1, &name);
-            // Bind the texture (make it the active one)
-            bind();
-            // Set the texture parameters for wrapping in the s and t direction to be repeat (texture will repeat itself)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            // Set the texture parameters for maginfying/minifying in the s and t direction to be linear interpolation (for better realstic results)
-            // we are using mipmaps so when miniifying we will lineary interpolate between the two closest mipmaps, and each mipmap value is also done by linear interpolation
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-            // note: mimpamp have no meaning when magnifying, so we use only GL_LINEAR
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            // Unbind the texture once we are done
-            unbind();
         };
 
         // This deconstructor deletes the underlying OpenGL texture
