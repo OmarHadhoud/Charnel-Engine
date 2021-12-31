4ac521bad1d7962d30869722e54b41ec166dc58b
Omar Salem Hadhoud <omarhadhoud@ymail.com>
2021-12-31 11:59:47 +0200

    add support for heart and coins

--- source/states/play-state.hpp
+++ source/states/play-state.hpp
@@ -21,6 +21,11 @@ class Playstate: public our::State {
     our::MovementSystem movementSystem;
     our::CollisionSystem collisionSystem;
     irrklang::ISoundEngine* soundEngine;
+    // the two entities representing the 2 digit score in the ui
+    our::Entity *score1;
+    our::Entity *score2;
+    // the score of the game
+    int score = 0;
 
     void onInitialize() override {
         std::cout << "hey there\n";
@@ -39,19 +44,40 @@ class Playstate: public our::State {
         soundEngine = irrklang::createIrrKlangDevice();
         soundEngine->play2D("assets/sound/theme.mp3", true);
         soundEngine->setSoundVolume(0.1f);
+
+        score1 = nullptr;
+        score2 = nullptr;
+        // get score digit entities
+        for (auto entity: world.getEntities())
+        {
+            if (entity->name == "score1")
+            {
+                score1 = entity;
+            }
+            else if(entity->name == "score2")
+            {
+                score2 = entity;
+            }
+            if (score1 != nullptr && score2 != nullptr)
+                break;
+        }
     }
 
     void onDraw(double deltaTime) override {
         // Here, we just run a bunch of systems to control the world logic
         movementSystem.update(&world, (float)deltaTime);
         cameraController.update(&world, (float)deltaTime);
+        score1->getComponent<our::MeshRendererComponent>()->material = our::AssetLoader<our::Material>::get(std::to_string(score/10));
+        score2->getComponent<our::MeshRendererComponent>()->material = our::AssetLoader<our::Material>::get(std::to_string(score%10));
         // And finally we use the renderer system to draw the scene
         auto size = getApp()->getFrameBufferSize();
         renderer.render(&world, glm::ivec2(0, 0), size);
         auto collisions = collisionSystem.update(&world, (float)deltaTime);
-        for(auto i: collisions)
+        // for testing purpose, increment the score when holding up key
+        auto app = getApp();
+        if(app->getKeyboard().isPressed(GLFW_KEY_UP))
         {
-            
+            score++;
         }
     }
 
